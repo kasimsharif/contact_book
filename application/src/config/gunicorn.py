592715ffa63c6672950eb5e849bbff9e4c09f4dd
Kasim Sharif <kasim.sharif@agrostar.in>
2019-09-11 12:58:26 +0530

    added gunicorn and supervisord

---
+++ application/src/config/gunicorn.py
@@ -0,0 +1,18 @@
+import os
+
+def numCPUs():
+    if not hasattr(os, "sysconf"):
+        raise RuntimeError("No sysconf detected.")
+    return os.sysconf("SC_NPROCESSORS_ONLN")
+
+bind = "0.0.0.0:7500"
+workers = numCPUs() * 2 + 1
+backlog = 2048
+worker_class ="gevent"
+debug = True
+daemon = False
+pidfile ="/tmp/contact-gunicorn.pid"
+logfile ="/tmp/contact-gunicorn.log"
+loglevel = 'info'
+accesslog = '/tmp/gunicorn-access.log'
+timeout = 120
